b76ef1877a7bea2b72781fc0f6c67b5b0bb0af43
Henry <128493575+ghnry1@users.noreply.github.com>
2023-06-14 08:48:29 -0300

    Add files via upload

--- Eventos.java
+++ Eventos.java
@@ -1,90 +1,158 @@
 package Entidades;
 
 public class Eventos {
- private int codigo;
- private String titulo;
- private String local;
- private int data;
- private String hora;
- private int qtdlugares;
- private int qtdlugaresvendidos;
- private double valoringresso;
- private String status;
- 
-public Eventos() {
-	super();
-}
-public Eventos(int codigo, String titulo, String local, int data, String hora, int qtdlugares, int qtdlugaresvendidos,
-		double valoringresso, String status) {
-	super();
-	this.codigo = codigo;
-	this.titulo = titulo;
-	this.local = local;
-	this.data = data;
-	this.hora = hora;
-	this.qtdlugares = qtdlugares;
-	this.qtdlugaresvendidos = qtdlugaresvendidos;
-	this.valoringresso = valoringresso;
-	this.status = status;
-}
-public int getCodigo() {
-	return codigo;
-}
-public void setCodigo(int codigo) {
-	this.codigo = codigo;
-}
-public String getTitulo() {
-	return titulo;
-}
-public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
-public String getLocal() {
-	return local;
-}
-public void setLocal(String local) {
-	this.local = local;
-}
-public int getData() {
-	return data;
-}
-public void setData(int data) {
-	this.data = data;
-}
-public String getHora() {
-	return hora;
-}
-public void setHora(String hora) {
-	this.hora = hora;
-}
-public int getQtdlugares() {
-	return qtdlugares;
-}
-public void setQtdlugares(int qtdlugares) {
-	this.qtdlugares = qtdlugares;
-}
-public int getQtdlugaresvendidos() {
-	return qtdlugaresvendidos;
-}
-public void setQtdlugaresvendidos(int qtdlugaresvendidos) {
-	this.qtdlugaresvendidos = qtdlugaresvendidos;
-}
-public double getValoringresso() {
-	return valoringresso;
-}
-public void setValoringresso(double valoringresso) {
-	this.valoringresso = valoringresso;
-}
-public String getStatus() {
-	return status;
-}
-public void setStatus(String status) {
-	this.status = status;
-}
- public int vendas(int qtdlugaresvendidos) {
-	 return qtdlugaresvendidos;
- }
- public int qtdlugareslivres(int qtdlugares) {
-	 return qtdlugares;
- }
+
+	private int codigo;
+	private String titulo;
+	private String local;
+	private String data;
+	private String hora;
+	private int qtdLugares;
+	private int qtdlugaresvendidos;
+	private double valoringresso;
+	private String status;
+	private static final double VALOR_DEFAULT_INGRESSO = 100;
+	private static int proximoCodigo = 1;
+    private static final int QTD_CAPACIDADE_DEFAULT = 3000;
+	private static final int QTD_INGRESSOS_VENDA_DEFAULT = 2;
+    public Eventos() {
+		super();
+		this.codigo= proximoCodigo++;
+		this.valoringresso = VALOR_DEFAULT_INGRESSO;
+		this.qtdLugares = QTD_CAPACIDADE_DEFAULT;
+		this.status = "Venda não iniciada";
+	}
+
+	public Eventos(int codigo, String titulo, String local, String data, String hora) {
+		super();
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.local = local;
+		this.data = data;
+		this.hora = hora;
+		this.qtdLugares = QTD_CAPACIDADE_DEFAULT;
+		this.qtdlugaresvendidos = QTD_INGRESSOS_VENDA_DEFAULT;
+		this.valoringresso = valoringresso;
+		this.status = "Venda não iniciada";
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public int getQtdlugares() {
+		return qtdLugares;
+	}
+
+	public void setQtdlugares(int qtdlugares) {
+		this.qtdLugares = qtdlugares;
+	}
+
+	public int getQtdlugaresvendidos() {
+		return qtdlugaresvendidos;
+	}
+
+	public void setQtdlugaresvendidos(int qtdlugaresvendidos) {
+		this.qtdlugaresvendidos = qtdlugaresvendidos;
+	}
+
+	public double getValoringresso() {
+		return valoringresso;
+
+	}
+
+	public void setValoringresso(double valoringresso) {
+		this.valoringresso = valoringresso;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void vendas(int quantidade) {
+		if(status.equals("Vendas iniciadas") && qtdlugaresvendidos + quantidade <= qtdLugares) {
+			qtdlugaresvendidos += quantidade;
+			System.out.println("Venda realizada!");
+		}else {
+			System.out.println("Não foi possível realizar a venda.");
+		}
+		
+	}
+	public void vendas() {
+		 vendas(QTD_INGRESSOS_VENDA_DEFAULT);
+	}
+	public int qtdlugareslivres(int qtdlugares) {
+		return qtdlugares-qtdlugaresvendidos;
+	}
+	public void iniciarvenda() {
+			if(status.equals("Vendas não iniciadas")) {
+				status = "Vendas iniciadas";
+				System.out.println("As vendas para o ingresso foram iniciadas.");
+			}else {
+				System.out.println("As vendas já foram iniciadas ou encerradas.");
+			}
+			}
+	public void finalizarvenda() {
+		if(status.equals("Vendas iniciadas")) {
+			status = "Vendas finalizadas.";
+		System.out.println("As vendas do evento foram finalizadas.");
+		}else {
+			System.out.println("As vendas não foram iniciadas ou já foram finalizadas.");
+		}
+	}
+	public void cancelarevento() {
+		if(!status.equals("Evento cancelado")) {
+    		status = "Evento cancelado";
+    		}else {
+    			System.out.println("O evento foi cancelado.");
+    		}
+	}
+    public void eventoocorrido() {
+    	if(!status.equals("Evento cancelado.") && !status.equals("Evento ocorrido.")){
+    		status = "Evento ocorrido.";
+    		System.out.println("Evento já ocorreu.");
+    	}else {
+    		System.out.println("O evento foi cancelado ou já ocorreu.");
+    	}
+    }
 }
---
+++ InterEventos.java
@@ -0,0 +1,183 @@
+package Aplicações;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import Entidades.Eventos;
+
+public class InterEventos {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		List<Eventos> evento = new ArrayList<>();
+
+		int qtdEventos = 0;
+
+		int opc;
+
+		do {
+			System.out.println("========MENU========");
+			System.out.println("Escolha uma opção:");
+			System.out.println("1. Cadastrar um Evento.");
+			System.out.println("2. Remover um Evento.");
+			System.out.println("3. Consultar os dados de um determinado evento.");
+			System.out.println("4. Iniciar a venda de ingressos para um determinado evento");
+			System.out.println("5. Encerrar a venda de ingressos para um determinado evento. ");
+			System.out.println("6. Cancelar um determinado evento.");
+			System.out.println("7. Marcar um determinado evento como já ocorrido.");
+			System.out.println("8. Consultar a quantidade de lugares livres para um determinado evento.");
+			System.out.println("9. Vender ingresso de um determinado evento.");
+			System.out.println("10. Listar todos os dados dos eventos.");
+			System.out.println("11. Sair");
+
+			opc = in.nextInt();
+
+			switch (opc) {
+			case 1:
+				System.out.println("\nCadastro de Evento:");
+				System.out.println("Digite o código do evento: ");
+				int codigo = in.nextInt();
+				System.out.println("Digite o título do evento: ");
+				String titulo = in.next();
+				System.out.println("Digite o local do evento: ");
+				String local = in.next();
+				System.out.println("Digite a data do evento: ");
+				String data = in.next();
+				System.out.println("Digite a hora do evento: ");
+				String hora = in.next();
+				evento.add(new Eventos(codigo, titulo, local, data, hora));
+				System.out.println("Evento cadastrado.");
+				continue;
+
+			case 2:
+				System.out.println("Digite o código do evento que gostaria de remover: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						evento.remove(i);
+
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 3:
+				System.out.println("Digite o código do evento que gostaria de consultar: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("O código é: " + evento.get(i).getCodigo() + "\no título é: "
+								+ evento.get(i).getTitulo() + "\no local é: " + evento.get(i).getLocal()
+								+ "\na data é: " + evento.get(i).getData() + "\na hora é: " + evento.get(i).getHora());
+
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+
+				continue;
+
+			case 4:
+				System.out.println("Digite o código do evento para iniciar a venda de ingressos: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("A venda de ingressos foi iniciada.");
+
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 5:
+				System.out.println("Digite o código do evento para finalizar a venda de ingressos: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("A venda de ingressos foi encerrada.");
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 6:
+				System.out.println("Digite o código do evento que gostaria de cancelar: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("O evento foi cancelado.");
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 7:
+				System.out.println("Digite o código do evento que gostaria de marcar como expirado: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("O evento já ocorreu.");
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 8:
+				System.out.println(
+						"Digite o código do evento que gostaria de consultar a quantidade de lugares livres: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("A quantidade de lugares livres é: " + evento.get(i).getQtdlugares());
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 9:
+				System.out.println("Digite o código do evento para venda dos ingressos: ");
+				codigo = in.nextInt();
+				for (int i = 0; i < evento.size(); i++) {
+					if (evento.get(i).getCodigo() == codigo) {
+						System.out.println("Quantos ingressos serão comprados?");
+						int qtd = in.nextInt();
+						System.out.println("Foram vendidos: " + qtd + " ingressos.");
+						System.out.println("Sobraram apenas " + (evento.get(i).getQtdlugares() + - qtd) + " lugares livres.");
+					} else {
+						System.out.println("Evento não encontrado.");
+					}
+
+				}
+				continue;
+
+			case 10:
+				
+
+				
+				continue;
+
+			default:
+				System.out.println("Opção inválida.");
+				
+			}
+
+		} while (opc != 11);
+
+		in.close();
+	}
+
+}
